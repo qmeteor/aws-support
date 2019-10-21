5b6074c69806907fce97ba78ff67d368ea530c05
Bien Pham <pham_bv@hotmail.com>
2019-10-20 22:08:23 -0400

    added cfn that generates ec2

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aws cloudformation deploy \
-    --template-file ../output.yaml \
-    --stack-name shopbuddy \
-    --capabilities CAPABILITY_IAM
--- package.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-aws cloudformation package \
-    --template-file ../get-objects-vpc-mysql.yaml \
-    --s3-bucket shopbuddy-artifacts \
-    --output-template-file ../output.yaml
-
-
